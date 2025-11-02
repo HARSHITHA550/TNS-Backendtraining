decee794d6e6ca8f5d9251c4dadee442f3c062f2
lenovo <harshitha1464@gmail.com>
2025-11-02 06:14:12 -0800

    Class commit

---
+++ src/mypack/Mobile.java
@@ -0,0 +1,15 @@
+package mypack;
+//Demo of the structure
+public class Mobile {
+	//variables
+	String brand;
+	String model;
+    int ram;
+	
+	//Method
+	void introduce() {
+		System.out.println("Mobile brand:"+brand+",model: "+model+""+",with "+ram+"GB RAM");
+	}
+	
+
+}
