a4b753e887597985978371fb7a3ecb3a1146259b
lenovo <harshitha1464@gmail.com>
2025-11-13 20:44:54 -0800

    user-defined class commit

---
+++ src/arraydemo/Student.java
@@ -0,0 +1,16 @@
+package arraydemo;
+
+public class Student {
+		private String name;
+		private int id;
+		private double percentage;
+		public Student(String name,int id,double percentage) {
+			this.name=name;
+			this.id=id;
+			this.percentage=percentage;
+		}
+		
+	}
+
+
+
