53adee39a11c8560d67c286fb839d9419254a231
lenovo <harshitha1464@gmail.com>
2025-11-15 22:25:28 -0800

    without exception handling commit

---
+++ src/exceptionDemo/WithoutException.java
@@ -0,0 +1,12 @@
+package exceptionDemo;
+
+//program without exception handling
+public class WithoutException {
+
+	public static void main(String[] args) {
+		int arr[]= {10,20,30};
+		System.out.println(arr[5]);
+
+	}
+
+}
